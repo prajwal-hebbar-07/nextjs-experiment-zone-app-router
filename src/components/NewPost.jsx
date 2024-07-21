75aefbf367783a483c86a2bd8a53ada270ee1119
Hebbar <prajwalhebbaras@gmail.com>
2024-07-21 18:34:34 +0530

    refactor(state): lift state from NewPost to PostsLists and update Post component
    - Lifted state from NewPost component to PostsLists component for shared state management.
    - Added functions in PostsLists to handle changes for both info and name fields.
    - Passed state handling functions as props to NewPost for the onChange prop of textarea (info) and input (name).
    - Updated the name and info of the first Post component based on changes recorded in the NewPost component.
    - Ensured consistent state updates and data flow between NewPost and Post components.

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,23 +1,15 @@
-import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  const [enteredInfo, setEnteredInfo] = useState("");
-
-  function changeInfoHandler(event) {
-    setEnteredInfo(event.target.value);
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="info">Info</label>
-        <textarea id="info" rows={3} onChange={changeInfoHandler} required />
+        <textarea id="info" rows={3} onChange={props.onInfoChange} required />
       </p>
-      <p>{enteredInfo}</p>
       <p>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" onChange={props.onNameChange} required />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,28 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
+import { useState } from "react";
 
 function PostsList() {
+  const [enteredInfo, setEnteredInfo] = useState("Test Component");
+  const [enteredName, setEnteredName] = useState("Prajwal Hebbar A S");
+
+  function infoChangeHandler(event) {
+    setEnteredInfo(event.target.value);
+  }
+
+  function nameChangeHandler(event) {
+    setEnteredName(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onInfoChange={infoChangeHandler}
+        onNameChange={nameChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post name="Prajwal Hebbar A S" info="Test Component" />
+        <Post name={enteredName} info={enteredInfo} />
         <Post name="Subramanya Prabhu U S" info="Reusable Component" />
       </ul>
     </>
