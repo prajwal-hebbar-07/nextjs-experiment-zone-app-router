f068bd2b1145d5baccfb921e934ccfa746f1c0bb
Hebbar <prajwalhebbaras@gmail.com>
2024-07-21 14:12:15 +0530

    feat(component): add dynamic data display functionality in Post.jsx
    - Enhanced the Post component to dynamically display name based on random number generated using math's random operator.
    - Implemented conditional rendering to update the content displayed by the Post component.
    - Improved the component's interactivity and responsiveness to data changes.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
+const names = ["Prajwal Hebbar A S", "Subramanya Prabhu U S"];
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <h1>Prajwal Hebbar A S</h1>
+      <h1>{chosenName}</h1>
       <p>First React Component</p>
     </div>
   );
