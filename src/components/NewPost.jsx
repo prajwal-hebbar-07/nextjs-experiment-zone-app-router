25658ff5d2bb8ce16a4e8872665b16602898ca46
Hebbar <prajwalhebbaras@gmail.com>
2024-07-21 18:21:11 +0530

    feat(state): add useState hook to update and display textarea value
    - Implemented useState hook to manage the state of the textarea input.
    - Updated the value entered in the textarea and displayed it in a p tag next to the textarea for testing purposes.
    - Verified the functionality to ensure the state updates correctly and reflects the entered text.

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  const [enteredInfo, setEnteredInfo] = useState("");
+
+  function changeInfoHandler(event) {
+    setEnteredInfo(event.target.value);
+  }
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="info">Info</label>
-        <textarea id="info" rows={3} required />
+        <textarea id="info" rows={3} onChange={changeInfoHandler} required />
       </p>
+      <p>{enteredInfo}</p>
       <p>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" required />
