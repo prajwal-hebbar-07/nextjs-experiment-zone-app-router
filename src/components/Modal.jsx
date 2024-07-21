629d0be565f29bab0904d1f3d3e6ab959f35d2d6
Hebbar <prajwalhebbaras@gmail.com>
2024-07-21 21:10:34 +0530

    feat(ui): add modal component with backdrop and dialog
    - Added a modal component with two elements: a self-closing div for the backdrop and a dialog for modal content.
    - Styled the backdrop and dialog using classes from Modal.module.css.
    - Ensured the modal component provides a backdrop and a styled content area for improved user experience.

---
+++ src/components/Modal.jsx
@@ -0,0 +1,13 @@
+import classes from "./Modal.module.css";
+
+function Modal({ children }) {
+  return (
+    <>
+      <div className={classes.backdrop} />
+      <dialog className={classes.modal} open>
+        {children}
+      </dialog>
+    </>
+  );
+}
+export default Modal;
