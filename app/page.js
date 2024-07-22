e51b319832e73590bd0a0a665920f7c36b147b37
Hebbar <prajwalhebbaras@gmail.com>
2024-07-22 21:42:02 +0530

    fix(routing): replace anchor tag with Link component for SPA navigation
    - Replaced anchor tag with Link component from next/link in main page.js.
    - Improved navigation to the About page while maintaining the single page application behavior.
    - Enhanced user experience by leveraging Next.js Link component for client-side routing.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
@@ -5,7 +7,7 @@ export default function Home() {
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p>
-        <a href="/about">About Page</a>
+        <Link href="/about">About Page!</Link>
       </p>
     </main>
   );
