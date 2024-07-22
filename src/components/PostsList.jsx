fc48860cf30d970d932ada477028d5d8126574b2
Hebbar <prajwalhebbaras@gmail.com>
2024-07-22 12:06:55 +0530

    feat(data-loading): add loader function to Posts and update PostsList
    - Added a loader function in the Posts component to fetch posts data.
    - Removed isFetching and posts useState hooks from PostsList.
    - Updated PostsList to use useLoaderData from react-router-dom for fetching posts data.
    - Integrated the loader function in main.jsx for the path "/" with the Posts element.
    - Improved data fetching and state management by leveraging react-router-dom's loader functionality.

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,23 +1,9 @@
-import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 import Post from "./Post";
-import NewPost from "../routes/NewPost";
 import classes from "./PostsList.module.css";
-import Modal from "./Modal";
 
-function PostsList({}) {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const data = await response.json();
-      setPosts(data.posts);
-      setIsFetching(false);
-    }
-    fetchPosts();
-  }, []);
+function PostsList() {
+  const posts = useLoaderData();
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
@@ -32,26 +18,22 @@ function PostsList({}) {
 
   return (
     <>
-      {!isFetching ? (
-        <>
-          {posts.length > 0 ? (
-            <ul className={classes.posts}>
-              {posts.map((post, index) => (
-                <Post key={index} name={post.name} info={post.info} />
-              ))}
-            </ul>
-          ) : (
-            <div style={{ textAlign: "center", color: "white" }}>
-              <h2>There are no posts yet!</h2>
-              <p>Try to add some posts</p>
-            </div>
-          )}
-        </>
-      ) : (
-        <div style={{ textAlign: "center", color: "white" }}>
-          <p>Loading Posts...</p>
-        </div>
-      )}
+      (
+      <>
+        {posts.length > 0 ? (
+          <ul className={classes.posts}>
+            {posts.map((post, index) => (
+              <Post key={index} name={post.name} info={post.info} />
+            ))}
+          </ul>
+        ) : (
+          <div style={{ textAlign: "center", color: "white" }}>
+            <h2>There are no posts yet!</h2>
+            <p>Try to add some posts</p>
+          </div>
+        )}
+      </>
+      )
     </>
   );
 }
