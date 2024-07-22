3c608d56bfc483291d446a29c6449e1d3bde902e
Hebbar <prajwalhebbaras@gmail.com>
2024-07-22 08:41:01 +0530

    feat(ui): add loading state in PostsList during data fetch
    - Added a loading state in PostsList to indicate data fetching in progress.
    - Displayed a loading indicator until the data fetch from the backend is complete.
    - Updated the UI to prevent user confusion during the data retrieval process.
    - Enhanced user experience by providing clear feedback during data loading.

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,14 +6,16 @@ import Modal from "./Modal";
 
 function PostsList({ modalIsVisible, onClose }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
   let modalContent;
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const data = await response.json();
       setPosts(data.posts);
-      console.log(data.posts);
+      setIsFetching(false);
     }
     fetchPosts();
   }, []);
@@ -40,16 +42,24 @@ function PostsList({ modalIsVisible, onClose }) {
   return (
     <>
       {modalContent}
-      {posts.length > 0 ? (
-        <ul className={classes.posts}>
-          {posts.map((post, index) => (
-            <Post key={index} name={post.name} info={post.info} />
-          ))}
-        </ul>
+      {!isFetching ? (
+        <>
+          {posts.length > 0 ? (
+            <ul className={classes.posts}>
+              {posts.map((post, index) => (
+                <Post key={index} name={post.name} info={post.info} />
+              ))}
+            </ul>
+          ) : (
+            <div style={{ textAlign: "center", color: "white" }}>
+              <h2>There are no posts yet!</h2>
+              <p>Try to add some posts</p>
+            </div>
+          )}
+        </>
       ) : (
         <div style={{ textAlign: "center", color: "white" }}>
-          <h2>There are no posts yet!</h2>
-          <p>Try to add some posts</p>
+          <p>Loading Posts...</p>
         </div>
       )}
     </>
