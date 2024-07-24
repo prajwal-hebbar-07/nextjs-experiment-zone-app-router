1a82cdaa68854ca22ba7eec40bab59eb2f98b0b0
Hebbar <prajwalhebbaras@gmail.com>
2024-07-24 11:20:03 +0530

    style(ui): add styles for MainHeader in main-header.module.css
    - Created main-header.module.css file with styles for MainHeader component.
    - Assigned CSS classes from main-header.module.css to elements in MainHeader component.
    - Enhanced the appearance of MainHeader with appropriate styling.

--- app/_components/main-header.js
+++ app/_components/main-header.js
@@ -1,15 +1,16 @@
 import logoImg from "@/assets/logo.png";
 import Link from "next/link";
 
+import styles from "./main-header.module.css";
+
 export default function MainHeader() {
   return (
-    <>
-      <Link href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
-        <p>Foodie Website</p>
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        <img src={logoImg.src} alt="A plate with food on it" /> Foodie Website
       </Link>
 
-      <nav>
+      <nav className={styles.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
@@ -19,6 +20,6 @@ export default function MainHeader() {
           </li>
         </ul>
       </nav>
-    </>
+    </header>
   );
 }
