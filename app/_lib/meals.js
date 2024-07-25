1be357f3e6fd2fe1073c8e16971212ddbb707671
Hebbar <prajwalhebbaras@gmail.com>
2024-07-25 18:43:43 +0530

    feat: Add NotFound page with inline styled button for navigation
    - Added a NotFound page using the reserved file name not-found.js.
    - Implemented inline styling for a button to navigate back to the home page on click.

--- app/_lib/meals.js
+++ app/_lib/meals.js
@@ -4,6 +4,5 @@ const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  throw new Error("Failed to fetch");
   return db.prepare("SELECT * FROM meals").all();
 }
