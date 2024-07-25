809757d09b34c0dc412bba788e661a96dc9d9aa1
Hebbar <prajwalhebbaras@gmail.com>
2024-07-25 12:14:22 +0530

    feat: Update styles in meals/page.js and add MealsGrid and MealItem components
    - Updated styles in meals/page.js.
    - Added MealsGrid component to render an unordered list of meals.
    - Added MealItem component to render individual meal details and dynamic route to meal/[slug].
    - Added styles for MealsGrid in meals-grid.module.css.
    - Added styles for MealItem in meal-item.module.css.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
+import styles from "./page.module.css";
+import MealsGrid from "../_components/meals/meals-grid";
+
 export default function Meals() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious Meals created{" "}
+          <span className={styles.highlight}>by Hebbar!</span>
+        </h1>
+        <p>Choose your favorite recepie and cook it at home.</p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recepie</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
