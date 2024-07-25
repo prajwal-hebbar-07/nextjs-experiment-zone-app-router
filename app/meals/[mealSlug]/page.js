0c0449807a5c5df391c982e54871ff93eaf72630
Hebbar <prajwalhebbaras@gmail.com>
2024-07-25 20:33:22 +0530

    feat: Create dynamic individual meal page with styling
    - Added content for individual meal page inside [mealSlug] directory.
    - Styled the page using page.module.css in [mealSlug] directory.
    - Accessed mealSlug in page.js using params passed by Next.js.
    - Wrote a function in meals.js (in _lib folder) to fetch individual meal info from the database.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,31 @@
-export default function DynamicMeals() {
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getIndividualMeal } from "@/app/_lib/meals";
+
+export default function MealDetailsPahe({ params }) {
+  const meal = getIndividualMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
-    <main>
-      <h1>Dynamic Meals Page</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <p className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </p>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
