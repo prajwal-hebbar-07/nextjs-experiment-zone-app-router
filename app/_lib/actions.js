115d0459efdb87f15f51b4b25e2c7c67973f4868
Hebbar <prajwalhebbaras@gmail.com>
2024-07-26 14:58:06 +0530

    feat: Add server-side validation for creating or saving meals
    - Implemented simple server-side validation before saving or creating a new meal or favorite meal.
    - Added the validation logic to the server action on the share page.

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,10 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+function isValid(input) {
+  return !input || input.trim() === "";
+}
+
 export async function shareMeal(formData) {
   const meal = {
     creator: formData.get("name"),
@@ -13,6 +17,18 @@ export async function shareMeal(formData) {
     image: formData.get("image"),
   };
 
+  if (
+    isValid(meal.title) ||
+    isValid(meal.summary) ||
+    isValid(meal.instructions) ||
+    isValid(meal.creator) ||
+    isValid(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid Input");
+  }
   await saveMeal(meal);
   redirect("/meals");
 }
