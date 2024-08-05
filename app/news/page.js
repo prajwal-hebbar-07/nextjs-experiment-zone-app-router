736888a5172e49fae8617cd6cfe8551a1c332bd3
Hebbar <prajwalhebbaras@gmail.com>
2024-08-05 13:00:17 +0530

    feat(styling): add styling for various pages and dummy_news.js for data population
    - Added styling for different pages.
    - Created `dummy_news.js` file in the root folder.
    - Accessed JSON data from `dummy_news.js` to populate content on the screen.
    
    This commit enhances the visual presentation of the application and introduces a mock data source for development.

--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,22 @@
+import { DUMMY_NEWS } from "@/dummy_news";
 import Link from "next/link";
 
 export default function News() {
   return (
     <>
       <h2>News Page</h2>
-      <ul style={{ textDecoration: "none" }}>
-        <li>
-          <Link href="/news/1">First News Page</Link>
-        </li>
-        <li>
-          <Link href="/news/2">Second News Page</Link>
-        </li>
-        <li>
-          <Link href="/news/3">Third News Page</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <p>{newsItem.title}</p>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
