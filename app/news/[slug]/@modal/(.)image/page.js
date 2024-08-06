b24c51544caa87d395c24a5f67decbd6ce95b527
Hebbar <prajwalhebbaras@gmail.com>
2024-08-06 16:33:37 +0530

    feat(modal-navigation): use useRouter to navigate back on outside click
    - Utilized `useRouter` from `next/navigation` to handle navigation.
    - Implemented `router.back` to go back to the previous page when clicking outside the image in the modal.
    
    This commit improves modal functionality by enabling navigation back to the previous page on outside click.

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,8 +1,11 @@
-import { notFound } from "next/navigation";
+"use client";
+
+import { notFound, useRouter } from "next/navigation";
 
 import { DUMMY_NEWS } from "@/dummy_news";
 
 export default function InterceptedFullScreenImage({ params }) {
+  const router = useRouter();
   const { slug } = params;
   const newsItem = DUMMY_NEWS.find((item) => item.slug === slug);
 
@@ -11,7 +14,7 @@ export default function InterceptedFullScreenImage({ params }) {
   }
 
   return (
-    <div className="modal-backdrop">
+    <div className="modal-backdrop" onClick={router.back}>
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
