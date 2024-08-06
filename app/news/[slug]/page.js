e951a489af6fc0eba70251ca6a28bfa326618c38
Hebbar <prajwalhebbaras@gmail.com>
2024-08-06 15:10:38 +0530

    feat(news): add nested route for image display inside news/[slug]
    - Added `image` directory inside `news/[slug]` for nested routing.
    - Created `page.js` file in `image` directory to access `params.slug`.
    - Implemented logic to display the full image on the page when an image is clicked.
    
    This commit enhances the news section by allowing nested routing for image display based on the news slug.

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,5 +1,7 @@
-import { DUMMY_NEWS } from "@/dummy_news";
 import { notFound } from "next/navigation";
+
+import { DUMMY_NEWS } from "@/dummy_news";
+import Link from "next/link";
 
 export default function ({ params }) {
   const { slug } = params;
@@ -12,7 +14,9 @@ export default function ({ params }) {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
