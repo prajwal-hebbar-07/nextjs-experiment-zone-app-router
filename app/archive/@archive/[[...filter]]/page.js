f8833556ef46513e197bb7e2e5180a0e0a29b058
Hebbar <prajwalhebbaras@gmail.com>
2024-08-06 12:45:11 +0530

    feat(archive): handle routes for /archive, /archive/[year], and /archive/[year]/[month]
    - Added code to handle routing for three different conditions:
      - `/archive`: Lists the years and displays a fallback message if no news is selected.
      - `/archive/[year]`: Captures the year using `filter[0]` and displays news for the selected year.
      - `/archive/[year]/[month]`: Captures the month using `filter[1]` and displays news for the selected month.
    - Added content and conditional checks to ensure proper functionality for all three routes.
    
    This commit enhances the archive section by implementing detailed routing and handling for different levels of news selection.

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,24 +1,54 @@
 import Link from "next/link";
 
-import { getAvailableNewsYears, getNewsForYear } from "@/app/_lib/news";
+import NewsList from "@/app/_components/news-list";
+
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/app/_lib/news";
 
 export default function FilteredNewsPage({ params }) {
   const { filter } = params;
-  console.log(filter);
+  let links = getAvailableNewsYears();
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+  let news;
+  let newsContent = <p>No year selected yet!</p>;
 
-  const links = getAvailableNewsYears();
+  if (selectedYear && selectedYear.length > 0) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
 
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
 }
