1ba29061bbf953e73113ecde1d1198e64d86872a
Hebbar <prajwalhebbaras@gmail.com>
2024-08-07 10:17:23 +0530

    feat(loader): add intentional delay to test loading state and implement fallback loader
    - Added an intentional delay in the `getAllNews` function using a new Promise and `setTimeout` to simulate loading.
    - Created `loading.js` reserved file in Next.js to display a fallback loader during the data fetching process.
    
    This commit enhances the user experience by testing and implementing a loading state with a fallback loader.

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -2,7 +2,7 @@ import NewsList from "@/app/_components/news-list";
 import { getAllNews } from "@/app/_lib/news";
 
 export default async function News() {
-  const news = getAllNews();
+  const news = await getAllNews();
 
   return (
     <>
