44d3355ca480f6ce75be84ca74fff8145981286c
Hebbar <prajwalhebbaras@gmail.com>
2024-08-07 09:20:50 +0530

    feat(news): implement fetch call with useEffect and useState for news data
    - Used `fetch` to call the local backend served on `http://localhost:8080/news`.
    - Implemented `useEffect` to fetch news data when the component mounts.
    - Added `useState` to manage loading and error states.
    - Displayed the UI based on loading, error, and fetched data states.
    
    This commit enhances the news component by integrating client-side data fetching and state management.

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import NewsList from "@/app/_components/news-list";
-import { DUMMY_NEWS } from "@/dummy_news";
 
 export default function News() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [news, setNews] = useState();
+
+  let newsContent;
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Failed to fetch the news!");
+        setIsLoading(false);
+      }
+
+      const news = await response.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h2>News Page</h2>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
