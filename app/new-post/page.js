98ea071c4fe2beb908f882a575daaff610f8fd96
Hebbar <prajwalhebbaras@gmail.com>
2024-08-08 10:57:56 +0530

    refactor(post): restructure server actions and refactor NewPostPage
    - Created an `actions` folder in the root directory.
    - Added `posts.js` file inside the `actions` folder and moved the server action from `NewPostPage` to this file.
    - Converted `NewPostPage` to a client-side component.
    - Moved the code back from `PostForm` component to `NewPostPage`.
    - Removed `PostForm` component as it is no longer needed.
    
    This commit organizes server actions in a dedicated folder and simplifies the project structure by refactoring components.

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,39 +1,46 @@
-import { redirect } from "next/navigation";
+"use client";
 
-import { storePost } from "@/lib/posts";
-import PostForm from "@/components/post-form";
+import { useFormState } from "react-dom";
+
+import FormStatus from "@/components/form-status";
+
+import { createPost } from "@/actions/posts";
 
 export default function NewPostPage() {
-  async function createPost(prevState, formData) {
-    "use server";
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
-    let errors = [];
+  const [state, formAction] = useFormState(createPost, {});
 
-    if (!title || title.trim().length === 0) {
-      errors.push("Title is required!");
-    }
-    if (!content || content.trim().length === 0) {
-      errors.push("Content is required!");
-    }
-    if (!image || image.size === 0) {
-      errors.push("Image is required!");
-    }
-
-    if (errors.length > 0) {
-      return { errors };
-    }
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/feed");
-  }
-
-  return <PostForm action={createPost} />;
+  return (
+    <>
+      <h1>Create a new post</h1>
+      <form action={formAction}>
+        <p className="form-control">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" />
+        </p>
+        <p className="form-control">
+          <label htmlFor="image">Image URL</label>
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            id="image"
+            name="image"
+          />
+        </p>
+        <p className="form-control">
+          <label htmlFor="content">Content</label>
+          <textarea id="content" name="content" rows="5" />
+        </p>
+        <p className="form-actions">
+          <FormStatus />
+        </p>
+      </form>
+      {state.errors && (
+        <ul className="form-errors">
+          {state.errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
