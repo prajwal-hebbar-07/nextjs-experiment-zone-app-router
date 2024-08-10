291d39c72f5167cef798fe3c6ccb0ec6270995e8
Hebbar <prajwalhebbaras@gmail.com>
2024-08-10 15:58:04 +0530

    feat(cache): integrate unstable_cache from next/cache for data caching
    This commit utilizes the `unstable_cache` function from `next/cache` to manage and optimize data caching, enhancing performance and reducing unnecessary data fetches.

--- app/messages/page.js
+++ app/messages/page.js
@@ -5,7 +5,7 @@ import { unstable_noStore } from "next/cache";
 // export const revalidate = 5;
 // export const dynamic = "force-dynamic";
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
   // unstable_noStore();
   // const response = await fetch("http://localhost:8080/messages", {
   //   next: {
@@ -15,7 +15,7 @@ export default function MessagesPage() {
   //     "X-ID": "page",
   //   },
   // });
-  const messages = getMessages();
+  const messages = await getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
